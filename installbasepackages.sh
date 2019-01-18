5539b2e62d7c890771f034a0c99f2b8616304568
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-01-18 18:32:28 -0500

    Update installbasepackages.sh

--- installbasepackages.sh
+++ installbasepackages.sh
@@ -6,4 +6,4 @@ wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.inputrc
 wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
 wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
 
-sudo apt-get -y install bash-completion vim curl
+sudo apt-get -o APT::Install-Suggests="true" -y install bash-completion vim curl
