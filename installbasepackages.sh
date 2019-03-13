1fa4ef10cbb7124209a0c04b3b4c80cc47f39cf1
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-03-13 12:38:25 -0500

    Add ll alias

--- installbasepackages.sh
+++ installbasepackages.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-cd 
+cd
 
-wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.inputrc
-wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
-wget https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
+wget -O ${HOME}/.inputrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.inputrc
+wget -O ${HOME}/.vimrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
+wget -O ${HOME}/.toprc https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
 
+sudo apt-get -y -q update
 sudo apt-get -y install bash-completion vim curl
+
+sed -i "s/#alias ll/alias ll/" ${HOME}/.bashrc
