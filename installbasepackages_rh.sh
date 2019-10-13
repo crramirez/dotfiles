57b736f739900e419627380a4ce75001210dca63
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2019-10-13 16:51:21 +0200

    Fix yum update

--- installbasepackages_rh.sh
+++ installbasepackages_rh.sh
@@ -6,7 +6,7 @@ wget -O ${HOME}/.inputrc https://raw.githubusercontent.com/crramirez/shellprefs/
 wget -O ${HOME}/.vimrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
 wget -O ${HOME}/.toprc https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
 
-sudo yum -y -q update
+sudo yum -y update
 sudo yum -y install bash-completion vim curl
 
 sed -i "s/#alias l/alias l/" ${HOME}/.bashrc
