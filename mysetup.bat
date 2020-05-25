8be40ce40b4eef548e2f1f80151ff13c58f2453c
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2020-05-25 16:34:19 +0200

    Create mysetup.bat

---
+++ mysetup.bat
@@ -0,0 +1,9 @@
+winget install Ditto
+winget install -e --id GIMP.GIMP
+winget install VcXsrv
+winget install Inkscape
+winget install WinMerge
+winget install Microsoft.Teams
+winget install Telegram.TelegramDesktop
+
+
