8eb3493c4dd9a6b23bdf0fb5ac3e98d2e862100d
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-03-31 11:55:09 +0200

    Update installbasepackages.sh

--- installbasepackages.sh
+++ installbasepackages.sh
@@ -5,6 +5,7 @@ cd
 wget -O ${HOME}/.inputrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.inputrc
 wget -O ${HOME}/.vimrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
 wget -O ${HOME}/.toprc https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
+wget -O ${HOME}/.screenrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.screenrc
 
 sudo apt-get -y -q update
 sudo apt-get -y install bash-completion vim curl
