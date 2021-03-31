e51e2514151c9099411ebe6dd3ea8def0d09fb76
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2021-03-31 11:54:53 +0200

    Update installbasepackages_rh.sh

--- installbasepackages_rh.sh
+++ installbasepackages_rh.sh
@@ -3,8 +3,9 @@
 cd
 
 wget -O ${HOME}/.inputrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.inputrc
-wget -O ${HOME}/.vimrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc
+wget -O ${HOME}/.vimrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.vimrc_rh
 wget -O ${HOME}/.toprc https://raw.githubusercontent.com/crramirez/shellprefs/master/.toprc
+wget -O ${HOME}/.screenrc https://raw.githubusercontent.com/crramirez/shellprefs/master/.screenrc
 
 sudo yum -y update
 sudo yum -y install bash-completion vim curl
