0916419253e04a41e8d339b32622ba8ed5dc4744
Carlos Rafael Ramirez <crramirez@users.noreply.github.com>
2022-05-16 10:19:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 cd
 
-ln -s .inputrc ${HOME}/.inputrc
-ln -s .vimrc ${HOME}/.vimrc
-ln -s .toprc ${HOME}/.toprc
-ln -s .screenrc ${HOME}/.screenrc
+rm -f .inputrc && ln -s .inputrc ${HOME}/.inputrc
+rm -f .vimrc && ln -s .vimrc ${HOME}/.vimrc
+rm -f .toprc && ln -s .toprc ${HOME}/.toprc
+rm -f .screenrc && ln -s .screenrc ${HOME}/.screenrc
 
 sudo apt-get -y -q update
 sudo apt-get -y install bash-completion vim curl
+
+sudo yum -y update
+sudo yum -y install bash-completion vim curl
 
 sed -i "s/#alias l/alias l/" ${HOME}/.bashrc
 
